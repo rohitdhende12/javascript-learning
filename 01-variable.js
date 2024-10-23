83d595c97651e9f41a7fbb7dc4275e153006184e
rohitdhende12 <rohitdhende1209@gmail.com>
2024-10-23 21:00:40 +0530

    variable assign file added
    variable assign file added

---
+++ 01-variable.js
@@ -0,0 +1,17 @@
+var city;
+console.log("city:", city);
+
+var village;
+console.log("variable village value is:", village);
+city="Pune";
+console.log("city value:", city);
+city="Mumbai";
+console.log("city value:", city);
+var fullName = "Jenny Gates";
+console.log("Full Name:", fullName);
+
+var pin = "411052";
+console.log("Pin:", pin);
+
+
+
---
+++ 01-variableAssign.js
@@ -0,0 +1,38 @@
+var myFullName;
+console.log("Full Name :", myFullName);
+
+myFullName = "Rohit Dhende";
+console.log("Full Name :", myFullName);
+
+myFullName = "Rohit Ashok Dhende";
+console.log("Full Name :", myFullName);
+
+var myLove = "I love only JS";
+console.log(myLove);
+
+var firstName = "Rohit";
+console.log("First Name :", firstName);
+
+var lastName = "Dhende";
+console.log("Last Name :", lastName);
+
+var address = "Karve Nagar, Pune";
+console.log("Address :", address);
+
+var pinCode = "411052";
+console.log("Pin Code :", pinCode);
+
+var gender = "Male";
+console.log("Gender :", gender);
+
+var isMarried = "Yes";
+console.log("Married :", isMarried);
+
+
+
+
+
+
+
+
+
