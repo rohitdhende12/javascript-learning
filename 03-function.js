5e204c8fc6326f274a7ee7a373340f2fcf9d37d4
rohitdhende12 <rohitdhende1209@gmail.com>
2024-11-07 18:06:18 +0530

    added function

---
+++ 03-function.js
@@ -0,0 +1,43 @@
+// Type 1: Function with no argument and no return type
+
+// Function Declaration
+
+function show(){
+
+    console.log("Inside show function...");
+
+    console.log("================");
+
+}
+
+// Function call or Invocation
+
+show();
+
+show();
+
+show();
+
+ 
+
+// Type 2: Function with argument and no return type
+
+function square(num){
+
+    console.log("Given number to square: ", num);
+
+    var result = num * num;
+
+    console.log("Square is: ", result);
+
+    console.log("----------------------------");  
+
+}
+
+square(2);
+
+square(5);
+
+square(19);
+
+square(190890);
